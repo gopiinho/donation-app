0e9921633948e5434d0c7bf19f382b23e32a7d4e
Gurpreet <gs109690@gmail.com>
2022-09-15 19:05:48 -0500

    donation tranaction through webpage working somewhat

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,9 @@
 import { ethers } from "./ethers-5.6.esm.min.js"
 import { abi } from "./constants.js"
 
+let provider = new ethers.providers.Web3Provider(window.ethereum)
+let signer
+
 const connectButton = document.getElementById("connectButton")
 const donateButton = document.getElementById("donateButton")
 
@@ -8,11 +11,20 @@ connectButton.onclick = connect
 donateButton.onclick = donate
 
 async function connect() {
-    if (typeof window.ethereum !== "undefined") {
-        await window.ethereum.request({ method: "eth_requestAccounts" })
-        connectButton.innerHTML = "Connected"
-    } else {
-        connectButton.innerHTML = "Install Metamask"
-    }
+    await provider.send("eth_requestAccounts", [])
+    signer = provider.getSigner()
+    connectButton.innerHTML = "Connected"
 }
-async function donate() {}
+
+async function donate() {
+    let ethAmount = document.getElementById("value").value
+
+    const donationAppContract = "0xffaA373d3863165190Ae70C8aE495058F0c41b7B" // Input contract address here.
+
+    const appContract = new ethers.Contract(donationAppContract, abi, provider)
+
+    const txResponse = await appContract
+        .connect(signer)
+        .doDonation("Gurpreet", "Testing", parseInt(ethAmount))
+    await txResponse.wait()
+}
